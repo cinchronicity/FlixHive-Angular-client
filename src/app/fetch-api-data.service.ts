adb454bdb844a572f1a3f137f7f334f868551b75
Cindy <168139730+cinchronicity@users.noreply.github.com>
2025-05-20 17:14:38 -0700

    Registration fails because the correct endpoint cannot be found

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -24,7 +24,7 @@ export class FetchApiDataService {
   // Making the api call for the user registration endpoint
   public userRegistration(userDetails: any): Observable<any> {
     console.log(userDetails);
-    return this.http.post(apiUrl + 'users', userDetails).pipe(
+    return this.http.post(apiUrl + '/users', userDetails).pipe(
       catchError(this.handleError)
     );
   }
